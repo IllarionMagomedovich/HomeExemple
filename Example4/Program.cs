7a5e7fcff9d3b2e2eb4a33bda8eafd96d4e5122b
IllarionMagomedovich <vadim.evenko98@mail.ru>
2022-11-15 00:52:14 +0300

    Add Example4

---
+++ Example4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: ");
+
+int numb = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+
+while(index <= numb)
+{
+    Console.WriteLine(index);
+    index = index + 2;
+}
